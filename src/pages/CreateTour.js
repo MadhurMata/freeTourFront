1439c5e3ee7dbc8daee0d21bd70e49951064da5d
Ismael Jaouhar <ismael.jaouhar@gmail.com>
2019-03-02 13:11:46 +0100

    Create page

--- src/pages/CreateTour.js
+++ src/pages/CreateTour.js
@@ -19,28 +19,42 @@ export default class CreateTour extends Component {
       return data  })
     .catch(error => console.log(error.response));
   };
- 
-   handleChange = event => {
+
+  handleChange = event => {
     let { name, value } = event.target;
     this.setState({ [name]: value });
   }
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleFormSubmit}>
+      <div className="create-box">
+        <form className="flex-column" onSubmit={this.handleFormSubmit}>
+        <label for="inp" class="inp">
+  <input type="text" id="inp" placeholder="&nbsp;"/>
+  <span class="label">Label</span>
+  <span class="border"></span>
+</label>
+          
+          <div className="flex-create">
           <label >Name:</label>
           <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
-          <label >Image:</label>
-          <input type="text" name="image" value={this.state.image} onChange={this.handleChange}/>
+          </div>
+          <div className="flex-create">
           <label >City:</label>
           <input type="text" name="city" value={this.state.city} onChange={this.handleChange}/>
+          </div>
+          <div className="flex-create">
+          <label >Image:</label>
+          <input type="text" name="image" value={this.state.image} onChange={this.handleChange}/>
+          </div>
+          <div className="flex-create">
           <label >Description:</label>
           <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
+          </div>
+          <div className="flex-create">
           <label >Duration:</label>
           <input type="text" name="duration" value={this.state.duration} onChange={this.handleChange}/>
-          <label >POI:</label>
-          <input type="text" name="POI" value={this.state.POI} onChange={this.handleChange}/>
+          </div>
           <button type="submit" value="submit">Add tour</button>
         </form>
       </div>
