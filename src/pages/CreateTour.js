e92c00fe1a68a5a97707e136410fa0cd5f53ca2e
pablograu <paugf89@gmail.com>
2019-03-02 14:56:30 +0100

    lol

--- src/pages/CreateTour.js
+++ src/pages/CreateTour.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import tourService from '../lib/tour-service'
+import React, { Component } from "react";
+import tourService from "../lib/tour-service";
 
 export default class CreateTour extends Component {
   state = {
@@ -10,55 +10,84 @@ export default class CreateTour extends Component {
     location: "",
     duration: "",
     POI: []
-  }
+  };
 
   handleFormSubmit = event => {
     event.preventDefault();
-    tourService.create(this.state)
-    .then((data) => {
-      console.log(data)
-      return data  })
-    .catch(error => console.log(error.response));
+    tourService
+      .create(this.state)
+      .then(data => {
+        console.log(data);
+        return data;
+      })
+      .catch(error => console.log(error.response));
   };
 
   handleChange = event => {
-  let { name, value } = event.target;
-  this.setState({ [name]: value });
-  }
+    let { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
       <div className="create-box">
         <form className="flex-column" onSubmit={this.handleFormSubmit}>
-        <label for="inp" class="inp">
-  <input type="text" id="inp" placeholder="&nbsp;"/>
-  <span class="label">Label</span>
-  <span class="border"></span>
-</label>
-          
+          <label for="inp" class="inp">
+            <input type="text" id="inp" placeholder="&nbsp;" />
+            <span class="label">Label</span>
+            <span class="border" />
+          </label>
+
           <div className="flex-create">
-          <label >Name:</label>
-          <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="flex-create">
-          <label >City:</label>
-          <input type="text" name="city" value={this.state.city} onChange={this.handleChange}/>
+            <label>City:</label>
+            <input
+              type="text"
+              name="city"
+              value={this.state.city}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="flex-create">
-          <label >Image:</label>
-          <input type="text" name="image" value={this.state.image} onChange={this.handleChange}/>
+            <label>Image:</label>
+            <input
+              type="text"
+              name="image"
+              value={this.state.image}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="flex-create">
-          <label >Description:</label>
-          <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
+            <label>Description:</label>
+            <input
+              type="text"
+              name="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="flex-create">
-          <label >Duration:</label>
-          <input type="text" name="duration" value={this.state.duration} onChange={this.handleChange}/>
+            <label>Duration:</label>
+            <input
+              type="text"
+              name="duration"
+              value={this.state.duration}
+              onChange={this.handleChange}
+            />
           </div>
-          <button type="submit" value="submit">Add tour</button>
+          <button type="submit" value="submit">
+            Add tour
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
