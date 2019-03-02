ac05ca96f4561ad87d963f853a05cdbbf16dde59
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-03-02 13:14:37 +0100

    home-page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { withAuth } from '../components/AuthProvider';
 import Navbar from '../components/Navbar';
+import BottomBar from '../components/BottomBar';
 import Tour from '../components/Tour';
 import tourService from '../lib/tour-service'
 import {BrowserRouter as Router, Switch, Link} from 'react-router-dom';
@@ -35,7 +36,6 @@ class Home extends Component {
     return (
       <div>
         <Navbar data='data' />
-        <h1>Welcome {user.username}</h1>
         <div>
          {tours.map((tour, id) => {
            console.log(tours)
@@ -48,7 +48,8 @@ class Home extends Component {
            /></Link>
           );
           })}
-        </div>
+        </div >
+        <BottomBar data='data' />
       </div>
     )
   }
