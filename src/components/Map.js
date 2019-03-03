d6605a9ca1e5b9fbe7ee748730e4d53e81f45586
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-03-03 17:15:41 +0100

    merge mapseachbar working

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,77 +1,96 @@
-import React, {Component} from 'react';
-import mapboxgl from 'mapbox-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import MapGL from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+import Geocoder from "react-map-gl-geocoder";
 
-export default class Map extends Component {
+// Please be a decent human and don't abuse my Mapbox API token.
+// If you fork this sandbox, replace my API token with your own.
+// Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
+const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoic21peWFrYXdhIiwiYSI6ImNqcGM0d3U4bTB6dWwzcW04ZHRsbHl0ZWoifQ.X9cvdajtPbs9JDMG-CMDsA";
 
-geolocate = new mapboxgl.GeolocateControl({
-  positionOptions: {
-    enableHighAccuracy: true
-  },
-  trackUserLocation: true
-});
-
-
-getRoute(map) {
-  var start = [2.154007, 41.390205];
-  var url =
-    `https://api.mapbox.com/directions/v5/mapbox/cycling/2.154007%2C41.390205%3B2.132000%2C41.380000%3B2.153007%2C41.390105%3B2.151007%2C41.390005.json?steps=true&geometries=geojson&access_token=` + mapboxgl.accessToken;
-  fetch(url)
-    .then((response) => {
-      response.json()
-        .then((data) => {
-          var data = data.routes[0];
-          var route = data.geometry;
-          console.log(route.coordinates)
-          map.on('load', function () {
-            map.addLayer({
-              "id": "route",
-              "type": "line",
-              "source": {
-                "type": "geojson",
-                "data": {
-                  "type": "Feature",
-                  "properties": {},
-                  "geometry": {
-                    "type": "LineString",
-                    "coordinates": route.coordinates
-                  }
-                }
-              },
-              "layout": {
-                "line-join": "round",
-                "line-cap": "round"
-              },
-              "paint": {
-                "line-color": "#888",
-                "line-width": 8
-              }
-            });
-          });
-        })
-    })
-}
-
-componentDidMount() {
-
-
-  const mapConfig = {
-    container: 'map',
-    style: 'mapbox://styles/ismaeljaouhar/cjsmz0aze8ddo1fmqhzvogy4g',
-    center: [2.15, 41.39],
-    zoom: 9,
+class App extends Component {
+  state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    },
+    searchResultLayer: null
   };
 
-  mapboxgl.accessToken = "pk.eyJ1IjoiaXNtYWVsamFvdWhhciIsImEiOiJjanMzZDBobzYwaHZ0NDNwbXlhdHM5eDF2In0.PT_A0flp8x4mH78w-JOegA";
+  mapRef = React.createRef();
 
-  this.map = new mapboxgl.Map(mapConfig);
+  componentDidMount() {
+    window.addEventListener("resize", this.resize);
+    this.resize();
+  }
 
-  this.getRoute(this.map);
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resize);
+  }
+
+  resize = () => {
+    this.handleViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
+  handleViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  };
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  handleGeocoderViewportChange = viewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    });
+  };
+
+  handleOnResult = event => {
+    this.setState({
+      searchResultLayer: new GeoJsonLayer({
+        id: "search-result",
+        data: event.result.geometry,
+        getFillColor: [255, 0, 0, 128],
+        getRadius: 1000,
+        pointRadiusMinPixels: 10,
+        pointRadiusMaxPixels: 10
+      })
+    });
+  };
+
+  render() {
+    const { viewport, searchResultLayer } = this.state;
+
+    return (
+      <MapGL
+        ref={this.mapRef}
+        {...viewport}
+        onViewportChange={this.handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <Geocoder
+          mapRef={this.mapRef}
+          onResult={this.handleOnResult}
+          onViewportChange={this.handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+        />
+        <DeckGL {...viewport} layers={[searchResultLayer]} />
+      </MapGL>
+    );
+  }
 }
 
-render() {
-
-  return ( <div className = 'map'
-    id = 'map' > </div>
-  );
-}
-}
+render(<App />, document.getElementById("root"));
