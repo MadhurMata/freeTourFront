cb13932b747f26050f6036dcdf6e0bf10b4fb994
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-03-05 12:01:13 +0100

    merge pau

--- src/components/TourRoute.js
+++ src/components/TourRoute.js
@@ -26,8 +26,7 @@ export default class Map extends Component {
             center: [2.15, 41.39],
             zoom: 13,
           };
-          mapboxgl.accessToken='pk.eyJ1IjoiaXNtYWVsamFvdWhhciIsImEiOiJjanMzZDBobzYwaHZ0NDNwbXlhdHM5eDF2In0.PT_A0flp8x4mH78w-JOegA';
-          
+          mapboxgl.accessToken = 'pk.eyJ1IjoiaXNtYWVsamFvdWhhciIsImEiOiJjanMzZDBobzYwaHZ0NDNwbXlhdHM5eDF2In0.PT_A0flp8x4mH78w-JOegA';
           this.map = new mapboxgl.Map(mapConfig);
           this.getRoute(this.map);
           this.geolocate = new mapboxgl.GeolocateControl({
@@ -49,7 +48,7 @@ export default class Map extends Component {
 
   paintPoints =() =>{
     const { tours } = this.state;
-    console.log(tours, "en paintPoints")
+    console.log(tours)
   }
 
   getRoute(map) {
@@ -58,8 +57,9 @@ export default class Map extends Component {
     // const lat = tours[10].POI[0].listOfPoi.lat
     // const lon = tours[10].POI[0].listOfPoi.lon
     // console.log(lat)
+
     var url =
-      `https://api.mapbox.com/directions/v5/mapbox/cycling/2.154007%2C41.390205%3B2.132000%2C41.380000%3B2.153007%2C41.390105%3B2.151007%2C41.390005.json?steps=true&geometries=geojson&access_token=${process.env.MAPBOX_KEY}`;
+      `https://api.mapbox.com/directions/v5/mapbox/cycling/2.154007%2C41.390205%3B2.132000%2C41.380000%3B2.153007%2C41.390105%3B2.151007%2C41.390005.json?steps=true&geometries=geojson&access_token=` + mapboxgl.accessToken;
     fetch(url)
       .then((response) => {
         response.json()
