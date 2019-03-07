771babfd4b8442db1e55b368810ec54230245d7e
Ismael Jaouhar <ismael.jaouhar@gmail.com>
2019-03-07 09:23:39 +0100

    Maps finished,

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import TourDetail from './pages/TourDetail';
 import ParentCreate from './pages/ParentCreate';
 import TourNavigator from './pages/TourNavigator';
 import AuthProvider from './components/AuthProvider';
-import Ratings from './components/Ratings';
+// import Ratings from './components/Ratings';
 import EditProfile from './pages/EditProfile';
 import Profile2 from './pages/Profile2';
 import Edit from './pages/Edit';
@@ -28,7 +28,7 @@ class App extends Component {
             <AnonRoute exact path="/login" component={Login} />
             <PrivateRoute exact path="/" component={Home} />
             <PrivateRoute exact path="/Profile2" component={Profile2} />
-            <PrivateRoute exact path="/Ratings" component={Ratings} />
+            {/* <PrivateRoute exact path="/Ratings" component={Ratings} /> */}
             <PrivateRoute exact path="/user/profile" component={Profile} />
             <PrivateRoute exact path="/tour/create" component={ParentCreate} />
             <PrivateRoute exact path="/tour/:id/navigator" component={TourNavigator} />
